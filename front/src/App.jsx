b7a16ae66dfee6acedb18fb5fa3808a58866d9a9
behrouzyou <behrouzyou@gmail.com>
2026-05-30 13:27:19 +0330

    transfer to bulmacss

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import '../node_modules/bulma/css/bulma.css'
+import Login from './admin/auth/Login'
+
+
+function App() {
+  return (
+    <>
+        <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/admin/context/context.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext();
+
+
+export const AuthContextProvider = ({children}) => {
+
+     
+
+     return (
+          <AuthContext.Provider value="">
+               {children}
+          </AuthContext.Provider>
+     )
+}
